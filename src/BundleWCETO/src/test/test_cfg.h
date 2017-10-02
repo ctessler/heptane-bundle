46d8d6b6d63b796ce4ed3ded7fe4412a9790e3f0
Corey Tessler <corey.tessler@wayne.edu>
2017-10-02 10:13:59 -0400

    Added the FunctionCall to the test_cfg

--- src/BundleWCETO/src/test/test_cfg.h
+++ src/BundleWCETO/src/test/test_cfg.h
@@ -5,6 +5,33 @@
 #include <lemon/list_graph.h>
 using namespace lemon;
 
+/**
+ * FunctionCall -- Represents the callsite for each instruction
+ */
+class FunctionCall {
+public:
+	FunctionCall(string name="UNASSIGNED", iaddr_t call_site=0);
+	FunctionCall(const FunctionCall &other);
+	bool operator==(const FunctionCall &other) const;
+	bool operator!=(const FunctionCall &other) const { return !(*this == other); }
+	FunctionCall& operator=(const FunctionCall &other);
+	friend std::ostream &operator<< (std::ostream &stream,
+					 const FunctionCall& fcall);
+
+	string getName() const;
+	void setName(string name="UNASSIGNED");
+
+	iaddr_t getCallSite() const;
+	void setCallSite(iaddr_t call_site=0);
+	static bool test();
+
+private:
+	/* Function name */
+	string _function_name;
+	/* Call site address of the function */
+	iaddr_t _call_addr;
+};
+
 class CFG : public ListDigraph {
 public:
 	CFG() : ListDigraph() { }
