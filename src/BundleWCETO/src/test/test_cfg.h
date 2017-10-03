e74bd0d1e5e72a6bcd61dcc210470d625a54ea70
Corey Tessler <corey.tessler@wayne.edu>
2017-10-03 12:04:44 -0400

    Using separate FunctionCall object

--- src/BundleWCETO/src/test/test_cfg.h
+++ src/BundleWCETO/src/test/test_cfg.h
@@ -2,41 +2,16 @@
 #define TEST_CFG_H
 
 
-typedef unsigned long iaddr_t;
-
 #include <lemon/core.h>
 #include <lemon/list_graph.h>
 
 #include <string>
+
+#include "BundleTypes.h"
+#include "FunctionCall.h"
+
 using namespace lemon;
 using namespace std;
-
-/**
- * FunctionCall -- Represents the callsite for each instruction
- */
-class FunctionCall {
-public:
-	FunctionCall(string name="UNASSIGNED", iaddr_t call_site=0);
-	FunctionCall(const FunctionCall &other);
-	bool operator==(const FunctionCall &other) const;
-	bool operator!=(const FunctionCall &other) const { return !(*this == other); }
-	FunctionCall& operator=(const FunctionCall &other);
-	friend std::ostream &operator<< (std::ostream &stream,
-					 const FunctionCall& fcall);
-
-	string getName() const;
-	void setName(string name="UNASSIGNED");
-
-	iaddr_t getCallSite() const;
-	void setCallSite(iaddr_t call_site=0);
-	static bool test();
-
-private:
-	/* Function name */
-	string _function_name;
-	/* Call site address of the function */
-	iaddr_t _call_addr;
-};
 
 class CFG : public ListDigraph {
 public:
