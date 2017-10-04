24485957c36472ff96e24d9a72b662b6c4e444ce
Corey Tessler <corey.tessler@wayne.edu>
2017-10-04 11:08:00 -0400

    Added the Cache test to remove the memory leak

--- src/BundleWCETO/src/test/CacheTest.cc
+++ src/BundleWCETO/src/test/CacheTest.cc
@@ -15,5 +15,45 @@ CacheTest::tearDown()
 void
 CacheTest::destructor()
 {
+	return;
+	PolicyLRU lru;
+	Cache original(32, 2, 32, 10, &lru);
+
+	original.insert(0x4000);
+}
+
+void
+CacheTest::setInsert()
+{
+	int line_size = 32;
+	
+	/* Test the method first */
+	map<uint32_t, CacheLine*> _storage;
+	iaddr_t addr= 0x4000;
+	int cache_line = addr % line_size;
+	CacheLine *cline = new CacheLine;
+	_storage.insert(make_pair(cache_line, cline));
+	_storage[cache_line]->resize(line_size);
+
+	map<uint32_t, CacheLine*>::iterator it;
+	for (it = _storage.begin(); it != _storage.end(); it++) {
+		delete it->second;
+	}
+	
+	PolicyLRU lru;
+	Cache original(32, 2, line_size, 10, &lru);
+	CacheSet set(&original);
+
+	set.insert(0x4000);
+	set.clear();
+}
+
+void
+CacheTest::lineInsert()
+{
+	int line_size = 32;
+	CacheLine line;
+	line.resize(line_size);
+	line.store(0x4000);
 }
 
--- src/BundleWCETO/src/test/CacheTest.h
+++ src/BundleWCETO/src/test/CacheTest.h
@@ -1,20 +1,23 @@
 #ifndef CACHETEST_H
 #define CACHETEST_H
-
-#include "CFG.h"
-#include "CFR.h"
-
 #include <cppunit/extensions/HelperMacros.h>
+
+#include "Cache.h"
+#include "PolicyLRU.h"
 
 class CacheTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(CacheTest);
 	CPPUNIT_TEST(destructor);
+	CPPUNIT_TEST(setInsert);
+	CPPUNIT_TEST(lineInsert);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void setUp();
 	void tearDown();
 
 	void destructor();
+	void setInsert();
+	void lineInsert();
 };
 
 #endif
