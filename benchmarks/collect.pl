985ac594d84677e3fb24c1cf492d374b59a40ee5
Corey Tessler <corey.tessler@wayne.edu>
2018-05-02 01:19:30 -0400

    Updated collect.pl to reflect the new context switch costs

--- benchmarks/collect.pl
+++ benchmarks/collect.pl
@@ -4,9 +4,10 @@ use strict;
 use Getopt::Long;
 use vars qw/$HDRFMT $LINEFMT/;
 Getopt::Long::Configure(qw{no_auto_abbrev no_ignore_case_always});
-
-$HDRFMT ="%-11s%-3s%-10s%-10s%-10s%-10s%-10s%-10s%-10s%-8s%-8s\n";
-$LINEFMT="%-11s%-3d%-10d%-10d%-10d%-10d%-10d%-10d%-10d%-8d%-8d\n";
+$HDRFMT  =      "%-11s". "%-3s%-4s%-3s" ."%-3s%-10s" ."%-10s";
+$HDRFMT .=      "%-10s". "%-10s" ."%-10s"."%-10s"."%-10s". "%-6s". "%-8s%-4s%-3s\n";
+$LINEFMT =      "%-11s". "%-3d%-4d%-3d" ."%-3d%-10d" ."%-10d";
+$LINEFMT.=      "%-10d". "%-10d" ."%-10d"."%-10d"."%-10d". "%-6d". "%-8d%-4d%-3d\n";
 
 sub usage {
 	return "collect.pl -t <NTHREADS> <BENCHMARK NAME> ...\n" .
@@ -22,20 +23,19 @@ sub main {
 	GetOptions("t=i", \$threads,
 		   "output|o=s", \$ofname)
 	    or die usage();
-	die usage() if (!defined($threads));
 
 	if (defined($ofname)) {
 		open($OFH, '>', $ofname) or die "Could not open $ofname";
 		select $OFH
 	}
 
-	my $ctxcost = get_ctx_cost(sprintf("%s/bundle/summary-%i.txt",
-					   $ARGV[0], $threads));
+	my ($bx, $tx);
+	($bx, $tx) = get_ctx_cost(sprintf("%s/bundle/summary.txt", $ARGV[0]));
 
-	print_hdr($ctxcost);
+	print_hdr($bx, $tx);
 	foreach my $mark (@ARGV) {
-		my $resname = sprintf("%s/bundle/summary-%i.txt", $mark, $threads);
-		proc_file($mark, $threads, $resname);
+		my $resname = sprintf("%s/bundle/summary.txt", $mark);
+		proc_file($mark, $resname);
 	}
 	
 	if (defined($ofname)) {
@@ -44,103 +44,139 @@ sub main {
 	}
 }
 sub print_hdr {
-	my $ctxcost = shift @_;
+	my ($bx, $tx);
+	($bx, $tx) = @_;
 	print
 	    "# Header Field Key:\n" .
 	    "# All execution times include the thread level context switch costs\n" .
-	    "# Thread level context switch cost: $ctxcost\n".
 	    "#\tMARK\t Benchmark name\n" .
 	    "#\tT\t Number of threads\n" .
-	    "#\tHWCET\t Heptane WCET\n" .
-	    "#\tI.1WO\t ILP v1.0 WCETO\n" .
-	    "#\tI.2WO\t ILP v2.0 WCETO\n" .
-	    "#\tH-I.2\t HWCET - I.2WO (the analytical benefit of BUNDLEP)\n" .
-	    "#\tBEXE\t Execution time in cycles under BUNDLEP\n" .
-	    "#\tHEXE\t Execution time in cycles under serialized execution\n" .
-	    "#\tH-E\t HEXE - BEXE (the run time benefit of BUNDLEP)\n" .
-	    "#\tCTXB\t Observed context switches for BUNDLEP\n" .
-	    "#\tCTXM\t Maximum length of a single context switch for HEXE = BEXE\n";
+	    "#\tB\t Block Reload Time\n" .
+	    "#\tI\t CPI\n" .
+	    "#\tS\t Number of Cache Sets\n" .
+	    "#\tHWCET\t Heptane WCET (W/ CTX)\n" .
+	    "#\tI.CWO\t Current ILP WCETO\n" .
+	    "#\tI.PWO\t Previous ILP WCETO\n" .
+	    "#\tH-I.C\t HWCET - I.CWO (the analytical benefit of BUNDLEP)\n" .
+	    "#\tHEXE\t Heptane execution time including CTX switches\n" .	    
+	    "#\tBEXE\t BUNDLEP execution time including CTX switches\n" .
+	    "#\tHE-BE\t HEXE - BEXE (the run time benefit of BUNDLEP)\n" .
+	    "#\tCTXB\t Observed bundle level context switches for BUNDLEP\n" .
+	    "#\tCTXT\t Observed thread level context switches for BUNDLEP\n" .
+	    "#\tBX\t Cycles per BUNDLE level context switch\n" .
+	    "#\tTX\t Cycles per thread level context switch\n";
 	#               %-8s      %3s  %10s     %10s     %10s     %10s
-	printf($HDRFMT, "# MARK", "T", "HWCET", "I.1WO", "I.2WO", "H-I.2",
-        #               %-10s   %-10s   %-10s  %-8     %-8
-	                "BEXE", "HEXE", "H-E", "CTXS", "CXTM");
-	
+	printf($HDRFMT, "# MARK", "T", "B", "I", "S", "HWECT", "I.CWO",
+	                "I.PWO", "H-I.C", "HEXE", "BEXE", "HE-BE", "CTXB", "CTXT",
+	#               %-10s    %-10s    %-10s   %-10s   %-10s    %-6s    %-8s
+	                "BX", "TX");
+	#               %-4s  %-3s
 }
 
 sub proc_file {
-	my ($mark, $threads, $file, $h, $dmatch);
+	my ($mark, $file, $h, $dmatch);
 	$dmatch='(\d+)\.{0,1}\d*';
-	($mark, $threads, $file) = @_;
+	($mark, $file) = @_;
 	open($h, '<', $file) or die "Could not open $file";
-	my ($ctx, $i1wceto, $i2wceto, $hwcet, $bexe, $bctx, $hexe);
+	my ($threads, $brt, $cpi, $sets, $hwcet, $ilpc, $ilpp, $hexe, $bexe, $ctxb, 
+	    $ctxt, $bx, $tx);
 	while (my $line = <$h>) {
-		my $regex = 'CTX:\s+' . "$dmatch";
+		my $regex = 'CACHE BRT:\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$ctx = $1;
+			$brt = $1;
 			next;
 		}
-		$regex = 'ILP 1.0 WCETO:\s+' . "$dmatch";
+		$regex = 'THREADS:\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$i1wceto = $1;
+			$threads = $1;
 			next;
 		}
-		$regex = 'ILP 2.0 WCETO:\s+' . "$dmatch";
+		$regex = 'CACHE CPI:\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$i2wceto = $1;
+			$cpi = $1;
 			next;
 		}
-		$regex = 'Heptane WCET:\s+' . "$dmatch" . '\s+\(1 thread\)';
+		$regex = 'CACHE SETS:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$sets = $1;
+			next;
+		}
+		$regex = 'Hept. WCET\(\d+ \+ CTXS\):\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
 			$hwcet = $1;
 			next;
 		}
-		$regex = 'Bundle Observed Execution Time:\s+' . "$dmatch";
+		$regex = 'ILP WCETO \(Current\):\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$bexe = $1;
+			$ilpc = $1;
 			next;
 		}
-		$regex = 'Bundle Thread Level Switches:\s+' . "$dmatch";
+		$regex = 'ILP WCETO \(Previous\):\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$bctx = $1;
+			$ilpp = $1;
 			next;
 		}
-		$regex = 'Serial Observed Execution Time:\s+' . "$dmatch";
+		$regex = 'Serial EXE\(W/ CTX\):\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
 			$hexe = $1;
 			next;
 		}
+		$regex = 'BUNDLE EXE\(W/ CTX\):\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$bexe = $1;
+			next;
+		}
+		$regex = 'N BUNDLE CTXs:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$ctxb = $1;
+			next;
+		}
+		$regex = 'N THREAD CTXs:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$ctxt = $1;
+			next;
+		}
+		$regex = 'BUNDLE CTX:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$bx = $1;
+			next;
+		}
+		$regex = 'THREAD CTX:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$tx = $1;
+			next;
+		}
 	}
-	foreach my $var ($ctx, $i1wceto, $i2wceto, $hwcet, $bexe, $bctx, $hexe) {
-		die "Unable to process file $file" if !defined($var);
+	foreach my $var ($brt, $cpi, $sets, $hwcet, $ilpc, $ilpp, $hexe, $bexe,
+			 $ctxb, $ctxt, $bx, $tx) {
+	    die "Unable to process file $file" if !defined($var);
 	}
-	# Calculate the maximum CTX switch for the two methods to be equal
-	my $slack = $hexe - $bexe;
-	
-	# Incorporate CTX costs
-	$hwcet = ($hwcet + $ctx) * $threads;
-	$bexe += $ctx * $bctx;
-	$hexe += $ctx * $threads;
-
-	printf($LINEFMT, $mark, $threads, $hwcet, $i1wceto, $i2wceto,
-	       $hwcet - $i2wceto, $bexe, $hexe, $slack, $bctx);
+	printf($LINEFMT, $mark, $threads, $brt, $cpi, $sets, $hwcet, $ilpc, $ilpp,
+	       $hwcet - $ilpc, $hexe, $bexe, $hexe - $bexe, $ctxb, $ctxt, $bx, $tx);
 	
 	close($h);
 }
 
 sub get_ctx_cost {
-	my ($ctx, $file, $dmatch, $h);
+	my ($bx, $tx, $file, $dmatch, $h);
 	$dmatch='(\d+)\.{0,1}\d*';	
 	$file = shift @_;
 	open($h, '<', $file) or die "Could not open $file";
 	while (my $line = <$h>) {
-		my $regex = 'CTX:\s+' . "$dmatch";
+		my $regex = 'BUNDLE CTX:\s+' . "$dmatch";
 		if ($line =~ /$regex/) {
-			$ctx = $1;
-			last;
+			$bx = $1;
+			next;
+		}
+		$regex = 'THREAD CTX:\s+' . "$dmatch";
+		if ($line =~ /$regex/) {
+			$tx = $1;
+			next;
 		}
 	}
 	close($h);
-	die "Unable to open read $file" if (!defined($ctx));
-	return $ctx;
+	die "Unable to open read $file" if (!defined($bx));
+	die "Unable to open read $file" if (!defined($tx));
+	return ($bx, $tx);
 }
 main(@ARGV);
