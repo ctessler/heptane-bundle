dbe2a2a615c831a33bbc81638bd0db189116e923
Corey <corey@n00b.net>
2019-01-03 12:32:41 -0500

    Fix variable usage in benchmark makefile

--- benchmarks/benchmark.mk
+++ benchmarks/benchmark.mk
@@ -1,10 +1,10 @@
 name=$(shell basename `pwd`)
-exe=${name}.exe
+exe=$(name).exe
 MIPSCC=$(shell command -v mips-cc 2>/dev/null)
 subdirs=bundle
 .PHONY: all clean $(subdirs) precheck
 
-all: ${name}.xml $(subdirs)
+all: $(name).xml $(subdirs)
 ifndef MIPSCC
 	$(error "mips-cc is not in the PATH")
 endif
@@ -12,21 +12,21 @@ endif
 $(subdirs): 
 	make -C $@ $(opt)
 
-${name}.xml: configExtract.xml
+$(name).xml: configExtract.xml
 	- ../../bin/HeptaneExtract configExtract.xml
 	@echo "Ignore 'Unknown annotation type found in binary 48' errors"
 
-${name}.exe: ${name}.c
-	mips-gcc -fomit-frame-pointer -ggdb -c -S ${name}.c -o ${name}.s
-	mips-as ${name}.s -o ${name}.o
-	mips-ld -e main -o ${name}.exe ${name}.o -T ../simul.ld
-	mips-readelf -S ${name}.exe > ${name}.readelf
-	mips-objdump -t -d -z ${name}.exe > ${name}.objdump
+$(name).exe: $(name).c
+	mips-gcc -fomit-frame-pointer -ggdb -c -S $(name).c -o $(name).s
+	mips-as $(name).s -o $(name).o
+	mips-ld -e main -o $(name).exe $(name).o -T ../simul.ld
+	mips-readelf -S $(name).exe > $(name).readelf
+	mips-objdump -t -d -z $(name).exe > $(name).objdump
 
 clean: opt=clean
 clean: $(subdirs)
-	rm -rf res*.xml *.pdf ${name}.xml *.log
-	rm -rf ${name}.dot ${name}.html ${name}.jpg core
-	rm -rf ${name}_*.xml annot.xml ${name}.o ${name}.s ${name}.objdump
-	rm -rf ${name}.readelf ${name}.exe
+	rm -rf res*.xml *.pdf $(name).xml *.log
+	rm -rf $(name).dot $(name).html $(name).jpg core
+	rm -rf $(name)_*.xml annot.xml $(name).o $(name).s $(name).objdump
+	rm -rf $(name).readelf $(name).exe
 
